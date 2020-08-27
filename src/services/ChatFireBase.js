0bce3f16aaf90bfd40e2f4af6fbf46456510edfc
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2020-08-27 19:47:07 +0500

    Firebase actual data implemented

--- src/services/ChatFireBase.js
+++ src/services/ChatFireBase.js
@@ -1,116 +1,108 @@
 /** @format */
 
 import * as firebase from 'firebase'
-var firebaseConfig = {
-    apiKey: "AIzaSyDMZjiJle6x-HE2pW2v5LACKo0gRrYG3rs",
-    authDomain: "chatproj-14b76.firebaseapp.com",
-    databaseURL: "https://chatproj-14b76.firebaseio.com",
-    projectId: "chatproj-14b76",
-    storageBucket: "chatproj-14b76.appspot.com",
-    messagingSenderId: "611764691528",
-    appId: "1:611764691528:web:bca232b3236dfcc916ae7d",
-    measurementId: "G-5HF9WFZQ98"
-  };
+import Config from '../config/config.json';
+var firebaseConfig = Config.firebaseConfig
 
 let firebaseApp = null
 if (firebase.apps.length == 0) {
-  firebaseApp = firebase.initializeApp(firebaseConfig)
+    firebaseApp = firebase.initializeApp(firebaseConfig)
 } else {
-  firebaseApp = firebase.apps[0]
+    firebaseApp = firebase.apps[0]
 }
 
 firebaseApp.getCurrentUser = function () {
-  return firebaseApp.auth().currentUser
+    return firebaseApp.auth().currentUser
 }
 
 firebaseApp.ref = () => {
-  return firebaseApp.database().ref()
+    return firebaseApp.database().ref()
 }
 
 firebaseApp.on = (callback) => {
-  return firebaseApp
-    .ref()
-    .child('chat')
-    .limitToLast(1)
-    .on('child_added', (snapshot) => {
-      // console.log('snapshot', snapshot)
-      callback(snapshot)
-    })
+    return firebaseApp
+        .ref()
+        .child('chat')
+        .limitToLast(1)
+        .on('child_added', (snapshot) => {
+            // console.log('snapshot', snapshot)
+            callback(snapshot)
+        })
 }
 
 firebaseApp.fetch = (author, callback) => {
-  return firebaseApp
-    .ref()
-    .child('users')
-    .child(author)
-    .once('value',
-      (snapshot) => callback(snapshot),
-      (errObj) => { })
+    return firebaseApp
+        .ref()
+        .child('users')
+        .child(author)
+        .once('value',
+            (snapshot) => callback(snapshot),
+            (errObj) => { })
 }
 
 firebaseApp.chats = (author, callback) => {
-  return firebaseApp
-    .ref()
-    .child('chat')
-    .child(author + '-' + 3)
-    .on('value', function (snapshot) {
-      if (snapshot.val() != null)
-        callback(snapshot)
-      else alert('No data ')
-    },
-      function (error) {
-        alert(error)
-      }
-    )
-  // .ref('/chat/' + author + '-' + 3).once('value').then(function (snapshot) {
-  //   if (snapshot.val() != null) {
-  //     callback(snapshot)
-  //   }
-  //   else {
-  //     alert('NochatsAvailable')
-  //   }
-  // });
-  // .ref()
-  // .child('chat')
-  // .child(author)
-  // .once('value', (snapshot) => callback(snapshot),
-  //   (errObj) => { }
-  // )
+    return firebaseApp
+        .ref()
+        .child('chat')
+        .child(author + '-' + 3)
+        .on('value', function (snapshot) {
+            if (snapshot.val() != null)
+                callback(snapshot)
+            else alert('No data ')
+        },
+            function (error) {
+                alert(error)
+            }
+        )
+    // .ref('/chat/' + author + '-' + 3).once('value').then(function (snapshot) {
+    //   if (snapshot.val() != null) {
+    //     callback(snapshot)
+    //   }
+    //   else {
+    //     alert('NochatsAvailable')
+    //   }
+    // });
+    // .ref()
+    // .child('chat')
+    // .child(author)
+    // .once('value', (snapshot) => callback(snapshot),
+    //   (errObj) => { }
+    // )
 
 }
 firebaseApp.chatForAdmin = (callback) => {
-  return firebaseApp
-    .ref()
-    .child('users')
-    .child(3)
-    .on('value', function (snapshot) {
-      if (snapshot.val() != null)
-        callback(snapshot)
-      else alert('No data ')
-    },
-      function (error) {
-        alert(error)
-      }
-    )
+    return firebaseApp
+        .ref()
+        .child('users')
+        .child(3)
+        .on('value', function (snapshot) {
+            if (snapshot.val() != null)
+                callback(snapshot)
+            else alert('No data ')
+        },
+            function (error) {
+                alert(error)
+            }
+        )
 
 
-  // ,
-  //   (snapshot) => callback(snapshot),
-  //   (errObj) => { })
-  //  firebaseApp
-  //   .ref('/users/3/').once('value').then(function (snapshot) {
-  //     if (snapshot.val() != null) {
-  //       callback(snapshot)
-  //     }
-  //     else {
-  //       alert('NochatsAvailable')
-  //     }
-  //   });
-  // .ref()
-  // .child('chat')
-  // .once('value', (snapshot) => callback(snapshot),
-  //   (errObj) => { }
-  // )
+    // ,
+    //   (snapshot) => callback(snapshot),
+    //   (errObj) => { })
+    //  firebaseApp
+    //   .ref('/users/3/').once('value').then(function (snapshot) {
+    //     if (snapshot.val() != null) {
+    //       callback(snapshot)
+    //     }
+    //     else {
+    //       alert('NochatsAvailable')
+    //     }
+    //   });
+    // .ref()
+    // .child('chat')
+    // .once('value', (snapshot) => callback(snapshot),
+    //   (errObj) => { }
+    // )
 }
 firebaseApp.off = () => firebaseApp.ref().off()
 
