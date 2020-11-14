fe7823394f76e548b739ae473653d438a82b5693
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2020-11-14 22:30:26 +0500

    Update style.js

--- src/screens/Barber/UpdateProfile/style.js
+++ src/screens/Barber/UpdateProfile/style.js
@@ -7,7 +7,7 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: THEME.PRIMARY_BACKGROUND_COLOR,
-        paddingTop: Platform.OS == 'ios' ? '25%' : '20%'
+        paddingTop: Platform.OS == 'ios' ? '30%' : '20%'
     },
     avatarStyle: {
         borderWidth: 1,
