569963c5ce1e11c14fa07295559d5a26940502f1
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-01-09 02:30:10 +0500

    hot fixes

--- src/screens/Customer/ServiceComplete/index.js
+++ src/screens/Customer/ServiceComplete/index.js
@@ -6,7 +6,8 @@ import THEME from '../../../assets/styles/theme.style';
 import Modal from 'react-native-modal'
 import StarRating from 'react-native-star-rating';
 import { Avatar } from "react-native-elements";
-import { KeyboardAwareView } from 'react-native-keyboard-aware-view'
+import DropDownPicker from 'react-native-dropdown-picker';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { Barbers } from '../../../services';
 import { connect } from 'react-redux';
 import moment from 'moment';
@@ -21,11 +22,13 @@ class ServiceComplete extends Component {
             barberName: '',
             barberAge: '',
             barberProfile: '',
-            tipModal: false,
+            tipModal: true,
             tip: '',
+            label: '',
             loading: false,
             giveTip: false,
-            finishLoading: false
+            finishLoading: false,
+            selectedTip: ''
         }
     }
     componentDidMount = () => {
@@ -55,7 +58,7 @@ class ServiceComplete extends Component {
         let { onHome } = this.props
         const { starCount, isFeedbackFocus, feedback,
             barberAge, barberName, barberProfile, tip, isTipFocus, giveTip, finishLoading,
-            tipModal, loading } = this.state;
+            tipModal, loading, label } = this.state;
         return (
             <View style={styles.container}>
                 {
@@ -65,7 +68,7 @@ class ServiceComplete extends Component {
                         </View>
                         :
                         <>
-                            <KeyboardAwareView>
+                            <KeyboardAwareScrollView style={{ flex: 1 }}>
                                 <View style={styles.upperContainer}>
                                     <View style={styles.cardStyle} >
                                         <View style={styles.avatarContainer}>
@@ -100,18 +103,33 @@ class ServiceComplete extends Component {
                                         </View>
                                     </View>
                                 </View>
-                            </KeyboardAwareView>
-                            <FooterButton disabled={feedback && starCount ? false : true} title='Done' onPress={() => {
 
-                                this.setState({ tipModal: true })
-                            }} />
+                                <View style={{ marginTop: '25%' }}>
+                                    <FooterButton disabled={feedback && starCount ? false : true} loading={finishLoading} title='Done' onPress={async () => {
+                                        this.setState({ finishLoading: true });
+                                        let userData = {
+                                            barber_id: this.props.userData.barber_id,
+                                            customer_id: this.props.userData.id,
+                                            comment: feedback,
+                                            no_of_star: starCount,
+                                            token: this.props.userData.token,
+                                            review_by: this.props.userData.type,
+                                            is_services_rate_time: moment().format("YYYY-MM-DD H:mm:ss"),
+                                            userName: this.props.user.userData.full_name,
+                                            booking_id: this.props.userData.booking_id
+                                        }
+                                        await onHome(userData);
+                                    }} />
+                                </View>
+
+                            </KeyboardAwareScrollView>
                             <Modal isVisible={tipModal}  >
                                 <View style={styles.content}>
                                     <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginBottom: '5%' }}>
                                         <View style={styles.iconContainer}>
                                             <Icon.FontAwesome5 name='hand-holding-usd' size={25} color={THEME.PRIMARY_COLOR} />
                                         </View>
-                                        <Text style={styles.headingText}>Give a tip to Barber!</Text>
+                                        <Text style={styles.headingText}>{giveTip ? "Give a tip to Barber!" : "Would you like to leave tip"}</Text>
                                     </View>
                                     {
                                         giveTip ?
@@ -126,38 +144,73 @@ class ServiceComplete extends Component {
                                                         <FloatingInput
                                                             val={tip}
                                                             keyboardtype="number-pad"
-                                                            onActive={() => this.setState({ isTipFocus: true })}
+                                                            onActive={() => this.setState({ isTipFocus: true, label: this.setState({ label: 'Tip' }) })}
                                                             onInActive={() => this.setState({ isTipFocus: false, submit: true })}
-                                                            label='$5' updateText={(tip) => this.setState({ tip })} />
+                                                            label={label != '' ? 'Tip' : '$5'} updateText={(tip) => this.setState({ tip })} />
                                                     </View>
                                                 </View>
 
-                                            </> : null}
+                                            </>
+                                            :
+                                            <View style={{ paddingBottom: '5%' }}>
+                                                <DropDownPicker
+                                                    items={[{
+                                                        id: 1,
+                                                        label: " 15%",
+                                                        value: `15%`
+                                                    },
+                                                    {
+                                                        id: 1,
+                                                        label: " 20%",
+                                                        value: `20%`
+                                                    },
+                                                    {
+                                                        id: 1,
+                                                        label: " 25%",
+                                                        value: `25%`
+                                                    }]}
+                                                    placeholder="Select a Tip"
+                                                    defaultValue={this.state.selectedTip ? this.state.selectedTip : null}
+                                                    containerStyle={{ height: 40 }}
+                                                    style={{ backgroundColor: '#fafafa' }}
+                                                    itemStyle={{
+                                                        justifyContent: 'flex-start'
+                                                    }}
+                                                    dropDownStyle={{ backgroundColor: '#fafafa' }}
+                                                    onChangeItem={(item) => this.setState({
+                                                        selectedTip: item.value,
+                                                    })}
+                                                />
+                                            </View>
+                                    }
                                     <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-                                        <TouchableOpacity onPress={() => this.setState({ giveTip: !giveTip })} style={styles.tipContainer}>
-                                            <Text style={styles.buttonText}>{giveTip ? 'Cancel' : 'Give Tip'}</Text>
+                                        <TouchableOpacity onPress={() => this.setState({ giveTip: !giveTip, label: '', tip: '' })} style={styles.tipContainer}>
+                                            <Text style={styles.buttonText}>{giveTip ? 'Cancel' : 'Custom Tip'}</Text>
                                         </TouchableOpacity>
                                         <TouchableOpacity onPress={async () => {
-                                            this.setState({ finishLoading: true });
-                                            let userData = {
-                                                barber_id: this.props.userData.barber_id,
-                                                customer_id: this.props.userData.id,
-                                                comment: feedback,
-                                                no_of_star: starCount,
-                                                token: this.props.userData.token,
-                                                review_by: this.props.userData.type,
-                                                is_services_rate_time: moment().format("YYYY-MM-DD H:mm:ss"),
-                                                userName: this.props.user.userData.full_name,
-                                                booking_id: this.props.userData.booking_id
-                                            }
-                                            await onHome(userData);
+                                            this.setState({ tipModal: false })
+                                            // this.setState({ finishLoading: true });
+                                            // let userData = {
+                                            //     barber_id: this.props.userData.barber_id,
+                                            //     customer_id: this.props.userData.id,
+                                            //     comment: feedback,
+                                            //     no_of_star: starCount,
+                                            //     token: this.props.userData.token,
+                                            //     review_by: this.props.userData.type,
+                                            //     is_services_rate_time: moment().format("YYYY-MM-DD H:mm:ss"),
+                                            //     userName: this.props.user.userData.full_name,
+                                            //     booking_id: this.props.userData.booking_id
+                                            // }
+                                            // await onHome(userData);
                                         }}
-                                            style={styles.tipContainer}>
+                                            style={[styles.tipContainer,
+                                                // {backgroundColor: tip||this.state.selectedTip? THEME.PRIMARY_COLOR:'#e2e2e2',}
+                                            ]}>
                                             {
                                                 finishLoading ?
                                                     <ActivityIndicator size={20} color={THEME.COLOR_WHITE} />
                                                     :
-                                                    <Text style={styles.buttonText}>Finish</Text>
+                                                    <Text style={styles.buttonText}>Done</Text>
                                             }
                                         </TouchableOpacity>
                                     </View>
