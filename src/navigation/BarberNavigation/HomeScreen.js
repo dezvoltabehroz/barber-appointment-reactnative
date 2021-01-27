65153df2b18b58b91d3c3fdb40dac3064ad42e16
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-01-27 15:00:58 +0500

    hot fixes

--- src/navigation/BarberNavigation/HomeScreen.js
+++ src/navigation/BarberNavigation/HomeScreen.js
@@ -25,8 +25,8 @@ class HomeScreen extends Component {
     }
 
     on_Press_Booking = (data, bookingId, customerId, bookingDate, bookingTime, bookingDuration) => {
-        const { push } = this.props.navigation
-        push('BarberServiceAccept', { item: data, bookingId, customerId, bookingDate, bookingTime, bookingDuration })
+        const { push,navigate } = this.props.navigation
+        navigate("Booking", { screen: 'BarberServiceAccept', params: { item: data, bookingId, customerId, bookingDate, bookingTime, bookingDuration } })
     }
 
     on_Press_Decline = (data, id) => {
