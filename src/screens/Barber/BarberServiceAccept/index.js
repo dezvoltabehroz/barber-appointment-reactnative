0aaf9d147ffb60861829fc0733451be329caa232
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-04-30 15:25:32 +0500

    Update index.js

--- src/screens/Barber/BarberServiceAccept/index.js
+++ src/screens/Barber/BarberServiceAccept/index.js
@@ -79,23 +79,23 @@ class BarberServiceAccept extends Component {
         let timeInHour = moment.utc().hours(h).minutes(m).format("HH:mm");
         let bookingEndTime = moment(date).add(timeInHour, 'hours')
         const enable = () => {
-            // if (moment(date).format('YYYY-MM-DD') == moment().format('YYYY-MM-DD')) {
-            //     if (moment().format('YYYY-MM-DD H:mm:ss') >= moment(date).format('YYYY-MM-DD H:mm:ss')) {
-            //         if (moment(bookingEndTime).format('YYYY-MM-DD H:mm:ss') >= moment().format('YYYY-MM-DD H:mm:ss')) {
-            //             return false;
-            //         }
-            //         else {
-            //             return true;
-            //         }
-            //     }
-            //     else {
-            //         return true;
-            //     }
-            // }
-            // else {
-            //     return true
-            // }
-            return false
+            if (moment(date).format('YYYY-MM-DD') == moment().format('YYYY-MM-DD')) {
+                if (moment().format('YYYY-MM-DD H:mm:ss') >= moment(date).format('YYYY-MM-DD H:mm:ss')) {
+                    if (moment(bookingEndTime).format('YYYY-MM-DD H:mm:ss') >= moment().format('YYYY-MM-DD H:mm:ss')) {
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+                }
+                else {
+                    return true;
+                }
+            }
+            else {
+                return true
+            }
+            // return false
         }
         return (
             <View style={styles.container}>
